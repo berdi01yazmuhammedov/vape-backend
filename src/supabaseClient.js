bde3db243ed170117352641be8c9244034c29b8d
Berdinazar <slybrd01@gmail.com>
2026-01-12 19:01:07 +0300

    fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL or Service Key is missing!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
