9c05e4712ac4d03c75c722af3c36a7711881ed15
Berdinazar <slybrd01@gmail.com>
2026-01-18 07:17:15 +0300

    fix: fixfix

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
