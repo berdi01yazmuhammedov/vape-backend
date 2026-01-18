1d12ea64e31433b4721f8f01cf6f19df14af7558
Berdinazar <slybrd01@gmail.com>
2026-01-18 07:42:54 +0300

    faidasndkqweqwe

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 const upload = multer({
-    storage: multer.memoryStorage()
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 export default upload;
