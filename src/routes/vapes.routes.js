cf8c102fe6fa8107eb2bc20ef891c92b0bd2e317
Berdinazar <slybrd01@gmail.com>
2026-01-18 07:18:08 +0300

    fixesfix

--- src/routes/vapes.routes.js
+++ src/routes/vapes.routes.js
@@ -2,7 +2,15 @@ import { Router } from "express";
 import upload from "../middlewares/upload.js";
 import { createVape, deleteVape, getVapes, updateVape } from "../controllers/vapes.controller.js";
 const router = Router();
-router.post("/", upload.single("image"), createVape);
+router.post(
+  "/",
+  (req, res, next) => {
+    console.log("POST /api/vapes HIT");
+    next();
+  },
+  upload.single("image"),
+  createVape
+);
 router.get("/", getVapes);
 router.delete("/:id", deleteVape);
 router.put("/:id", updateVape)
