1f068e003d66730b7512c357833a13e4fea931ed
Berdinazar <slybrd01@gmail.com>
2026-01-18 07:29:44 +0300

    fix: try fix to fixsadopiajwidjasnd

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
