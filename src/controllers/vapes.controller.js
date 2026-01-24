e6c8e92c122af352809cc749fea4b87e17d052b2
Berdinazar <slybrd01@gmail.com>
2026-01-24 06:58:27 +0300

    fix: try catch add

--- src/controllers/vapes.controller.js
+++ src/controllers/vapes.controller.js
@@ -1,61 +1,58 @@
 import { supabase } from "../supabaseClient.js";
-import path from "path"
+import path from "path";
 export const createVape = async (req, res) => {
-  const {
-    name,
-    price,
-    description,
-    brand,
-    flavor,
-    strength,
-    stock,
-    puffs,
-  } = req.body;
+  try {
+    const { name, price, description, brand, flavor, strength, stock, puffs } =
+      req.body;
 
-  if (!name || !price) {
-    return res.status(400).json({ message: "Нет названия или цены" });
-  }
+    if (!name || !price) {
+      return res.status(400).json({ message: "Нет названия или цены" });
+    }
 
-  let imageURL = null;
+    let imageURL = null;
 
-  if(req.file){
-    const ext = path.extname(req.file.originalname);
-    const fileName = `${Date.now()}${ext}`;
+    if (req.file) {
+      const ext = path.extname(req.file.originalname);
+      const fileName = `${Date.now()}${ext}`;
 
-    const {error} = await supabase.storage
-    .from("vapes")
-    .upload(fileName, req.file.buffer, {
-        contentType: req.file.mimetype,
-    });
+      const { error } = await supabase.storage
+        .from("vapes")
+        .upload(fileName, req.file.buffer, {
+          contentType: req.file.mimetype,
+        });
+
+      if (error) {
+        return res.status(500).json({ message: error.message });
+      }
+      imageURL = `${process.env.SUPABASE_URL}/storage/v1/object/public/vapes/${fileName}`;
+    }
+    const { data, error } = await supabase
+      .from("vapes")
+      .insert([
+        {
+          name,
+          price: Number(price),
+          description: description || "",
+          brand: brand || "",
+          flavor: flavor || "",
+          strength: strength || "",
+          stock: Number(stock) || 0,
+          puffs: Number(puffs) || 0,
+          image: imageURL,
+        },
+      ])
+      .select()
+      .single();
 
     if (error) {
-        return res.status(500).json({ message: error.message });
+      return res.status(500).json({ message: error.message });
     }
-    imageURL = `${process.env.SUPABASE_URL}/storage/v1/object/public/vapes/${fileName}`;
-  }
-  const { data, error } = await supabase
-    .from("vapes")
-    .insert([
-      {
-        name,
-        price: Number(price),
-        description: description || "",
-        brand: brand || "",
-        flavor: flavor || "",
-        strength: strength || "",
-        stock: Number(stock) || 0,
-        puffs: Number(puffs) || 0,
-        image: imageURL,
-      },
-    ])
-    .select()
-    .single();
 
-  if (error) {
-    return res.status(500).json({ message: error.message });
+    res.status(201).json(data);
+  } catch (error) {
+    console.error("CREATE VAPE CRASH:", err);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  res.status(201).json(data);
 };
 
 export const deleteVape = async (req, res) => {
